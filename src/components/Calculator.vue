475d4dfc2cbbeacee2400c6783bf0a32986f7e75
Simen Daehlin <simen.dehlin@gmail.com>
2018-08-18 13:15:41 +0100

    added operator methods

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -4,19 +4,19 @@
     <div @click="clear" class="btn">C</div>
     <div @click="sign" class="btn">+/-</div>
     <div @click="percent" class="btn">%</div>
-    <div class="btn operators">÷</div>
+    <div @click="divide" class="btn operators">÷</div>
     <div @click="append('7')" class="btn">7</div>
     <div @click="append('8')" class="btn">8</div>
     <div @click="append('9')" class="btn">9</div>
-    <div class="btn operators">x</div>
+    <div @click="times" class="btn operators">x</div>
     <div @click="append('4')" class="btn">4</div>
     <div @click="append('5')" class="btn">5</div>
     <div @click="append('6')" class="btn">6</div>
-    <div class="btn operators">-</div>
+    <div @click="minus" class="btn operators">-</div>
     <div @click="append('1')" class="btn">1</div>
     <div @click="append('2')" class="btn">2</div>
     <div @click="append('3')" class="btn">3</div>
-    <div class="btn operators">+</div>
+    <div @click="add" class="btn operators">+</div>
     <div @click="append('0')" class="btn zero">0</div>
     <div @click="dot()" class="btn">.</div>
     <div class="btn operators">=</div>
@@ -47,6 +47,15 @@ export default {
       if(this.current.indexOf('.') === -1) {
         this.append('.')
       }
+    },
+    divide() {
+      
+    },
+    times() {
+      
+    },
+    add() {
+      
     }
 	}
 };
