161bda259f0525b87ed6ab6c584ea883458f6bc5
Simen Daehlin <simen.dehlin@gmail.com>
2018-08-18 12:42:30 +0100

    Gutted template, init commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="calculator">
+
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.calculator {
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+}
+</style>
